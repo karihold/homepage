cb69e33dfe7f06280fc3c5a5576381a75d7b1f6a
karihold <holdcroftkari@gmail.com>
2022-06-14 17:05:57 +0200

    added structure, assets, fonts and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import ReactDom from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Frontpage from "pages/frontpage/frontpage";
+import Contact from "pages/contact/contact";
+import NotFound from "pages/not-found/not-found";
+import Header from "components/header/header";
+import Footer from "components/footer/footer";
 
 const renderTarget = document.querySelector("#render");
 
 const App = () => {
-  return <h1>yolo</h1>;
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" index element={<Frontpage />} />
+          <Route path="portfolio/:page" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
-ReactDom.render(<App />, renderTarget);
+render(<App />, renderTarget);
---
+++ src/js/pages/frontpage/frontpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./frontpage.scss";
+
+const Frontpage = () => {
+  return (
+    <>
+      <section className="frontpage-div">
+        <h1 className="frontpage-h1">Kari Holdcroft</h1>
+        <p>Frontend Developer</p>
+      </section>
+    </>
+  );
+};
+
+export default Frontpage;
