44c0dabdc28d05293ffcfef52ec102e0900d90dc
karihold <holdcroftkari@gmail.com>
2022-06-16 14:19:49 +0200

    added frontpage images

--- src/js/pages/frontpage/frontpage.jsx
+++ src/js/pages/frontpage/frontpage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
 import "./frontpage.scss";
+
 import PhoneImage from "images/phone.jpg";
+import ArtHistoryImage from "images/art-history.jpg";
+import ApartmentWebsiteImage from "images/apartment-website.jpg";
+import FinancePlatformImage from "images/finance-platform.jpg";
+import HolidazeImage from "images/holidaze.jpg";
+import RedPepperPizzaImage from "images/red-pepper-pizza.jpg";
+import DragonsAndDiceImage from "images/dragonsanddice.jpg";
+import SpaceXImage from "images/spacex.jpg";
+import ScienceMuseumImage from "images/science-museum.jpg";
+
 import FrontpageIllustration from "assets/illustrations/frontpage-illustration.svg";
 import DownArrowIcon from "assets/icons/down-arrow-icon.svg";
 
@@ -16,19 +26,22 @@ const Frontpage = () => {
         </h1>
         <p className="frontpage-title-p">Frontend Developer</p>
       </section>
-      <HashLink id="down-arrow-icon" smooth to="/#portfolio-project-section">
-        <img src={DownArrowIcon} />
-      </HashLink>
+      <section className="down-arrow-section">
+        <HashLink id="down-arrow-icon" smooth to="/#portfolio-project-section">
+          <img className="down-arrow-img" src={DownArrowIcon} />
+        </HashLink>
+      </section>
+
       <section id="portfolio-project-section">
         <PortfolioArticle
           mainTitle="ART HISTORY APP"
           subheading="UX Prototype"
           year="2022"
-          description="Art history app is created based on a UX-design prompt. The goal was to create an app that appeals to the
+          description="Art history app is created based on a UX-design prompt as part of Google UX Design Professional Certificate. The goal was to create an app that appeals to the
             broader public, but also caters to the users who use the gallery the most"
-          image={PhoneImage}
+          image={ArtHistoryImage}
           imageAlt="Art history app"
-          keywords={["kake", "bake"]}
+          keywords={["UX-design", "UI-design", "UX-research", "figma", "prototyping"]}
           caseStudy="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://kariholdcroft.com/case-study-art-history-app.pdf"
           prototype="https://www.figma.com/proto/wwYr6TQUnkF26TobuzP4OX/Art-History-App?node-id=155%3A680&scaling=min-zoom&page-id=154%3A127&starting-point-node-id=155%3A680&show-proto-sidebar=1"
         />
